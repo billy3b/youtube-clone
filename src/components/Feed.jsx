c4c129a2f994d9767502eb487a243855a697d1f5
billy3b <avinashroy45678@gmail.com>
2023-01-09 13:34:36 +0530

    created feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -8,11 +8,6 @@ const Feed = () => {
   const [selectedCategory, setSelectedCategory] = useState('New');
   const [videos, setVideos] = useState([]);
 
-  useEffect(() => {
-    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
-      .then((data) => setVideos(data.items))
-    },[selectedCategory]);
-    
 
   return (
     <Stack sx= {{flexDirection: {sx: 'column', md:'row'} }}>
@@ -25,12 +20,7 @@ const Feed = () => {
           Copyright 2023 Avinash Roy
         </Typography>
       </Box>
-      <Box p={2} sx={{overflowY: 'auto', height: '90vh', flex:2}}>
-        <Typography variant="h4" fontWeight='bold' mb={2} sx={{ color:'white'}}>
-          {selectedCategory} <span style={{color:'#F31503'}}>videos</span>
-        </Typography>
-        <Videos videos={videos}/>
-      </Box>
+      
     </Stack>
   )
 }
