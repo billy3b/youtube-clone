caea80fc1374807814d7242cf2a3eab76d7ccb3c
billy3b <avinashroy45678@gmail.com>
2023-01-10 13:45:41 +0530

    added videos to feed

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -23,7 +23,12 @@ const Feed = () => {
           Copyright 2023 Avinash Roy
         </Typography>
       </Box>
-      
+      <Box p={2} sx={{overflowY: 'auto', height: '90vh', flex:2}}>
+        <Typography variant="h4" fontWeight='bold' mb={2} sx={{ color:'white'}}>
+          {selectedCategory} <span style={{color:'#F31503'}}>videos</span>
+        </Typography>
+        <Videos videos={videos}/>
+      </Box>
     </Stack>
   )
 }
