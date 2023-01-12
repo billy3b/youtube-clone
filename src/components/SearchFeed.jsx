3a79948196e7dbb0a562cb57d4ab2ecaafbce11f
billy3b <avinashroy45678@gmail.com>
2023-01-12 13:54:00 +0530

    api call searchfeed component

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -6,13 +6,17 @@ import { useParams } from 'react-router-dom';
 
 const SearchFeed = () => {
   const [videos, setVideos] = useState([]);
+  const {searchTerm} =useParams();
 
+  useEffect(() => {
+    fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
+      .then((data) => setVideos(data.items))
+    },[]);
     
-
   return (
     <Box p={2} sx={{overflowY: 'auto', height: '90vh', flex:2}}>
         <Typography variant="h4" fontWeight='bold' mb={2} sx={{ color:'white'}}>
-          Search Result for: <span style={{color:'#F31503'}}>searchTerm</span> videos
+          Search Result for: <span style={{color:'#F31503'}}>{searchTerm}</span> videos
         </Typography>
         <Videos videos={videos}/>
     </Box>
