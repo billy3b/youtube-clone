ca381681d0e27cf42d82fa6577ddeed296d3d806
billy3b <avinashroy45678@gmail.com>
2023-01-12 14:04:51 +0530

    debugging searchfeed component

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -7,12 +7,13 @@ import { useParams } from 'react-router-dom';
 const SearchFeed = () => {
   const [videos, setVideos] = useState([]);
   const {searchTerm} =useParams();
-
+  
   useEffect(() => {
     fetchFromAPI(`search?part=snippet&q=${searchTerm}`)
       .then((data) => setVideos(data.items))
-    },[]);
+    },[searchTerm]);
     
+
   return (
     <Box p={2} sx={{overflowY: 'auto', height: '90vh', flex:2}}>
         <Typography variant="h4" fontWeight='bold' mb={2} sx={{ color:'white'}}>
