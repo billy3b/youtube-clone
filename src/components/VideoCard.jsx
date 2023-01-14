159d116139d57fff7f891330e92151b6b2956b3e
billy3b <avinashroy45678@gmail.com>
2023-01-14 14:13:05 +0530

    Created VideoCard component

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -5,7 +5,7 @@ import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import { demoThumbnailUrl, demoVideoUrl, demoVideoTitle, demoChannelUrl, demoChannelTitle } from '../utils/constants';
 
 
-const VideoCard = ({ video : {id: {videoId}, snippet }}) => {
+const VideoCard = ({ video : {id: videoId, snippet }}) => {
   console.log(videoId, snippet);
   return (
     <Card sx={{width:{xs:'100%', md:'320px',sm:'350px'}, boxShadow:'none', borderRadius:0}}>
@@ -17,20 +17,7 @@ const VideoCard = ({ video : {id: {videoId}, snippet }}) => {
         sx={{ width: { xs: '100%', sm: '360px',md:'320px'}, height: 180 }} 
       />
       </Link> 
-      <CardContent sx={{backgroundColor:'#1e1e1e', height: '106px'}}> 
-        <Link to= {videoId ? `/video/${videoId}` : demoVideoUrl}>
-          <Typography variant='subtitle1' fontWeight='bold' color='#FFF'>
-            {snippet.title.slice(0,60) || demoVideoTitle.slice(0,60)}
-          </Typography>
-        </Link>
-        <Link to= {snippet ?.channelId ? `/channel/${snippet?.channelId}` : demoVideoUrl}>
-          <Typography variant='subtitle2' fontweight='gray' color='#FFF'>
-            {snippet.channelTitle.slice(0,60) || demoChannelTitle.slice(0,60)}
-            <CheckCircleIcon sx={{fontSize:12, color:'gray', ml:'5px'}} />
-          </Typography>
-        </Link>
-      
-      </CardContent>
+
     </Card>
   )
 }
